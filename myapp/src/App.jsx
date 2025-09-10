a21d4d13472cdffc122dc6a9f80975fccc53917f
sasindaran s <19384119@pondiuni.ac.in>
2025-09-10 09:36:43 +0530

    09-09-25-class props

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,11 +1,12 @@
 import Student from "./Student";
-
+import Stud from "./Stud";
 
 function App()
 {
   return(
     <div>
-      <Student name="Itachi" age={21} isStudent={true}/>
+      {/* <Student name="Itachi" age={21} isStudent={true}/> */}
+      <Stud name="Itachi" age={21} isStudent={true}/>
     </div>
   );
 }
---
+++ myapp/src/Stud.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+class Stud extends Component
+{
+    render(){
+        return(
+            <>
+            <h1>{this.props.name}</h1>
+            </>
+        )
+    }
+}
+export default Stud;
