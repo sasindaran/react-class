656d96a852aa67dcb967c5b0529807299a93ddaf
sasindaran s <19384119@pondiuni.ac.in>
2025-09-10 09:47:36 +0530

    props

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -5,8 +5,11 @@ function App()
 {
   return(
     <div>
-      {/* <Student name="Itachi" age={21} isStudent={true}/> */}
-      <Stud name="Itachi" age={21} isStudent={true}/>
+      <Student name="Itachi" age={21} isStudent={true}/>
+      <Student name="Naruto" age={21} isStudent={true}/>
+      <Student name="Jiraiya" age={21} isStudent={true}/>
+      <Student name="Minato" age={21} isStudent={true}/>
+      {/* <Stud name="Itachi" age={21} isStudent={true}/> */}
     </div>
   );
 }
