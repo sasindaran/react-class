ae4907aec14517eec594a2aa65b5dc18ad33b3e0
sasindaran s <19384119@pondiuni.ac.in>
2025-09-11 09:42:58 +0530

    simple login green and red

--- myapp/src/Student.jsx
+++ myapp/src/Student.jsx
@@ -1,7 +1,8 @@
 export default function Student(info)
 {
     return(
-
-        info.isStudent?<h1>Welcome  to Skyllx {info.name}</h1>: <h1>User not Found</h1>
+        // if true display welcome-message
+        // if false display no-login
+        info.isStudent?<h1 className="welcome-message">Welcome  to Skyllx {info.name}</h1>: <h1 className="no-login">User not Found</h1>
     )
 }
