bf709279cc13b8d32b3939029eb57981563c5188
sasindaran s <19384119@pondiuni.ac.in>
2025-09-16 14:41:01 +0530

    sort function

--- myapp/src/List.jsx
+++ myapp/src/List.jsx
@@ -6,10 +6,12 @@ export default function List() {
                  {id: 2,name:"Orange",calories:80},
                  {id: 3,name:"Banana",calories:50},
                  {id: 4,name:"Cocount",calories:43},
-                 {id: 5,name:"Pineaaple",calories:25},
+                 {id: 5,name:"Pineapple",calories:25},
    ]
 
-   fruits.sort()
+   //If number use subtract symbol
+   //fruits.sort((a,b)=>{return a.name-b.name})
+   fruits.sort((a,b)=>a.name.localeCompare(b.name))
 
    const listItem=fruits.map(fruits=> <li key={fruits.id}>{fruits.name}:&nbsp;<b>{fruits.calories}</b></li>);
 
